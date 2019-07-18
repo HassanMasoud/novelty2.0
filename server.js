7e7e6ccbf0ac4ac13e925abdefc2b2b9c6a05de9
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-18 18:14:04 +0300

    author routes setup and delete partial

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -19,6 +20,7 @@ const bookRoute = require("./routes/books");
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
+app.use(methodOverride("_method"));
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(express.urlencoded({ limit: "10mb", extended: false }));
